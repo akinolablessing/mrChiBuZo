2be58a0b9ad99fd18c8c28e03297f937818297a7
akinolablessing <akinolablessing1890@gmail.com>
2024-12-03 14:50:17 +0100

    pattern

--- phaseGateTwo/StudentGrade.java
+++ phaseGateTwo/StudentGrade.java
@@ -1,5 +1,5 @@
 // I changed the code because is not giving me the result i want sir
-
+// sum/grade[1].length
 import java.util.Scanner;
 import java.util.Arrays;
 public class Correction{
@@ -12,8 +12,8 @@ int studentNumber = scanner.nextInt();
 System.out.print("How many subject do they offer?:");
 int studentSubject = scanner.nextInt();
 
-
- 
+int sum =0; 
+int average =0;
 int[][] scoresNumber = new int[studentNumber][studentSubject];
 int[] totalNumber = new int[studentNumber];
 int[] averageNumber = new int[studentNumber];
@@ -24,16 +24,23 @@ for(int count=0; count<studentNumber; count++){
 
  for(int index =0; index<studentSubject; index++){
     int score;
+    
       do{
        System.out.println("Enter score for subject:"+(index +1)+":");
        score = scanner.nextInt();
-      if(score < 0|| score > 1000){
+        sum+=score;
+         
+             if(score < 0|| score > 1000){
        System.out.print("Please enter a score between between 0 and 100");
    }
 
   }while(score < 0 || score > 100);
    scoresNumber[count][index] = score;
- }
+    
+      }
+  
+  System.out.println("The total of the score is:"+sum);
+    
 }
 }
 }
